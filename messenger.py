fbee78e59ad3f6cdb0ee37740faa12070de4f46a
unknown <eugenie.porre@etu.minesparis.psl.eu>
2024-11-05 16:39:21 +0100

    ajouter les messages

--- messenger.py
+++ messenger.py
@@ -14,10 +14,13 @@ server = {
             'reception_date': datetime.now(),
             'sender_id': 1,
             'channel': 1,
-            'content': 'Hi 👋'
+            'content': 'Hi'
         }
     ]
 }
+
+CHANNELS = [c['id'] for c in server['channels']]
+print(CHANNELS)
 
 def id_to_name(id):
     L = []
@@ -30,20 +33,41 @@ def id_to_name(id):
         print("ERREUR : Plusieurs utilisateurs correspondent à l'identifiant",id,"demandé")
         return L
 
-print('=== Messenger ===')
-print('x. Leave\nA. See users\nB. See channels')
-choice = input('Select an option: ')
-if choice == 'x':
-    print('Bye!')
-elif choice == 'A':
-    for u in server['users']:
-        print(u['id'],".",u['name'])
-elif choice == 'B':
-    for c in server['channels']:
-        print(c['id'],".",c['name'])
-        print('    Users:')
-        for id in c['member_ids']:
-            print('    -',id_to_name(id))
-else:
-    print('Unknown option:', choice)
 
+def messenger():
+    print('=== Messenger ===')
+    print('x. Leave\nA. See users\nB. See channels')
+    choice = input('Select an option: ')
+    if choice == 'x':
+        print('Bye!')
+        return None
+    elif choice == 'A':
+        for u in server['users']:
+            print(u['id'],".",u['name'])
+    elif choice == 'B':
+        for c in server['channels']:
+            print(c['id'],".",c['name'])
+            print('    Users:')
+            for id in c['member_ids']:
+                print('    -',id_to_name(id))
+        print("Would you like to see some channel messages ?\nYes/No")
+        choix = input('Select an option: ')
+        if choix == 'Yes':
+            group = input('Enter channel id: ')
+            if int(group) not in CHANNELS :
+                print('Unknown option:', group)
+            M = [m for m in server['messages'] if m['channel'] == int(group)]
+            for m in M:
+                print('************************************')
+                print("Message id :",m['id'])
+                print("Message sent by",id_to_name(m['sender_id']),"at",m['reception_date'],":")
+                print(m['content'])
+                print('************************************')
+        elif choix == 'No':
+            messenger()
+        else :
+            print('Unknown option:', choix)
+    else:
+        print('Unknown option:', choice)
+
+messenger()
