e1e91fae6b4ae5afbf79d7d716bd0eac65fc7e91
unknown <eugenie.porre@etu.minesparis.psl.eu>
2024-11-05 15:13:59 +0100

    menu affichage groupe

--- messenger.py
+++ messenger.py
@@ -19,10 +19,31 @@ server = {
     ]
 }
 
+def id_to_name(id):
+    L = []
+    for u in server['users']:
+        if u['id'] == id:
+            L.append(u['name'])
+    if len(L) == 1:
+        return L[0]
+    else :
+        print("ERREUR : Plusieurs utilisateurs correspondent à l'identifiant",id,"demandé")
+        return L
+
 print('=== Messenger ===')
-print('x. Leave')
+print('x. Leave\nA. See users\nB. See channels')
 choice = input('Select an option: ')
 if choice == 'x':
     print('Bye!')
+elif choice == 'A':
+    for u in server['users']:
+        print(u['id'],".",u['name'])
+elif choice == 'B':
+    for c in server['channels']:
+        print(c['id'],".",c['name'])
+        print('    Users:')
+        for id in c['member_ids']:
+            print('    -',id_to_name(id))
 else:
     print('Unknown option:', choice)
+
