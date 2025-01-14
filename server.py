4c864de02cab706b2cf70925cfe1d44db1830c5e
unknown <eugenie.porre@etu.minesparis.psl.eu>
2025-01-14 16:56:34 +0100

    remplacement server par remoteServer et localServer

--- server.py
+++ server.py
@@ -7,14 +7,13 @@ class Server :
         self.users = users
         self.channels = channels
         self.messages = messages
+        self.server = self
+    def get_users(self)->list[User]:
+        pass
     def server_to_dico(self)->dict:
-        users = [User.user_to_dico(user) for user in self.users]
-        channels = [Channel.channel_to_dico(channel) for channel in self.channels]
-        messages = [Message.message_to_dico(message) for message in self.messages]
-        return {'users': users,'channels':channels,'messages':messages}
+        print('ERROR')
+        return 'ERROR'
     @classmethod
     def dico_to_server(cls,server:dict)->'Server':
-        users = [User.dico_to_user(user) for user in server['users']]
-        channels = [Channel.dico_to_channel(channel) for channel in server['channels']]
-        messages = [Message.dico_to_message(message) for message in server['messages']]
-        return cls(users,channels,messages)
+        print('ERROR')
+        return 'ERROR'
