cb55f77a185a591dbdedcfd78b50f689035f3ef0
unknown <eugenie.porre@etu.minesparis.psl.eu>
2025-01-14 16:54:41 +0100

    ajout -f/-u

--- messenger.py
+++ messenger.py
@@ -1,53 +1,30 @@
-from datetime import datetime
-import json
-# ajout ligne parser
-import argparse
-parser=argparse.ArgumentParser(description="mise en entrée de paramètres json")
-parser.add_argument("--server", "-s", help="donne le chemin d'accès vers le fichier json")
-pars=parser.parse_args()
-nom_fichier_json=parser.server
+from argparse import ArgumentParser
 
-import json
+from server import Server
+from client import Client
+from localServer import LocalServer
+from remoteServer import RemoteServer
+
+argument_parser = ArgumentParser()
+argument_parser.add_argument('-f','--file')
+argument_parser.add_argument('-u','--url')
+arguments = argument_parser.parse_args()
+server : Server
+if arguments.file is not None :
+    server = LocalServer(arguments.file)
+elif arguments.url is not None :
+    server = RemoteServer(arguments.url)
+else :
+    print('error : -f or -u should be set, try again')
+    exit(-1)
 
 file_name = 'server.json'
 
-class Remoteserver :
-    def __init__(self,url:str):
-        self.url = url
-    def getserver(self):
+# class Remoteserver :
+#     def __init__(self,url:str):
+#         self.url = url
+#     def getserver(self):
 
-#Client.messenger()
-
-# server = {
-#     'users': [
-#         {'id': 1, 'name': 'Alice'},
-#         {'id': 2, 'name': 'Bob'}
-#     ],
-#     'channels': [
-#         {'id': 1, 'name': 'Town square', 'member_ids': [1, 2]}
-#     ],
-#     'messages': [
-#         {
-#             'id': 1,
-#             'reception_date': datetime.now(),
-#             'sender_id': 1,
-#             'channel': 1,
-#             'content': 'Hi'
-#         }
-#     ]
-# }
-
-# def open_server(file_name):
-#     with open(file_name) as json.file :
-#         return Server.dico_to_server(json.load(json.file))
-
-### 
-# Il faut aussi supprimer les messages du user
-# Il faut aussi supprimer le user des groupes
-###
-# def delete_users():
-#     N = [n.strip() for n in input('Enter users you wish to remove from the server -separated by commas:').split(',')]
-#     for n in N:
-#         if n not in [u['name'] for u in server['users']]:
-#             print("ERREUR : ",n,"is not an user")
-#         else :
+server.load()
+client = Client(server)
+client.messenger()
