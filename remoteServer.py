5166ef249ff1b4d5f2add46306bb7e59182c495b
unknown <eugenie.porre@etu.minesparis.psl.eu>
2025-01-21 17:04:31 +0100

    get_channels non fonctionnel

--- remoteServer.py
+++ remoteServer.py
@@ -19,6 +19,13 @@ class RemoteServer(Server) :
         Users = response.json()
         return [User.dico_to_user(user) for user in Users]
     
+    # def get_channels(self)->list[Channel]:
+    #     response = requests.get('https://groupe5-python-mines.fr/channels')
+    #     Channels = response.json()
+    #     for channel in Channels :
+    #         #
+    #     return [Channel.dico_to_channel(channel) for channel in Channels]
+    
     def create_user(self,new_user:dict):
         requests.post('https://groupe5-python-mines.fr/users/create',json = new_user)
         
